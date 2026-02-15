5d1ecaaf999b47e9159b348763cd9de8505e6f79
cheonaggwi <cheonaggwi@gmail.com>
2026-02-16 01:54:25 +0900

    v4 색상, 3웹 기초 구조

--- campusScript.js
+++ campusScript.js
@@ -1,3 +1,38 @@
+// 버튼 클릭 이벤트
+
+const buttonList = document.querySelectorAll('#buttonAll *');
+
+var buttonAppList = []
+
+for (const i of buttonList) {
+    if (i.id !== "button_2_1") {
+        buttonAppList.push([document.querySelector('#'+i.id+'_App'),i]);
+    }
+}
+
+for (const i of buttonAppList) {
+    i[1].addEventListener('click',function(){
+        i[0].style.display = 'block';
+        requestAnimationFrame(() => {
+            i[0].style.opacity = 1;
+            i[0].style.transform = 'scale(1)';
+            document.querySelector('#mainApp').style.transform = 'scale(1.05)';
+        });
+    })
+    document.querySelector('#'+i[0].id+' .exit').addEventListener('click',function(){
+        i[0].style.opacity = 0;
+        i[0].style.transform = 'scale(0.9)';
+        document.querySelector('#mainApp').style.transform = 'scale(1)';
+        setTimeout(() => {
+            i[0].style.display = 'none';
+        }, 150);
+    })
+}
+
+document.querySelector('#button_2_1').addEventListener('click',()=>{
+    window.open("https://login.donga.ac.kr/login?rd_c_p=checked", "_blank");
+})
+
 // 밑 버튼 클릭 시 화면 전환
 
 document.querySelector('#button_4_1').addEventListener('click',()=>{
@@ -6,8 +41,6 @@ document.querySelector('#button_4_1').addEventListener('click',()=>{
 document.querySelector('#button_4_3').addEventListener('click',()=>{
     window.location.assign('data.html')
 })
-
-
 
 // 화면 비율 정리
 
